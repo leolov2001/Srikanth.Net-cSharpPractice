10b8195a1f709ab195d8635f424fb37d8411665c
Sunny Leo M <sunny.leo@tammina.com>
2019-02-26 12:11:42 +0530

    Modified Ex. 3.1 with logic

--- NoOfDays.cs
+++ NoOfDays.cs
@@ -10,11 +10,38 @@ namespace cSharpDemo
     {
         static void Main(string[] args)
         {
-            int year; string month;
+            int year, month;
             Console.Write("Please enter the year: ");
             year = Int32.Parse(Console.ReadLine());
-            Console.Write("Please enter the month: ");
-            month = (Console.ReadLine());
+            Console.Write("Please enter the number of the month (01 - 12): ");
+            month = Int32.Parse(Console.ReadLine());
+
+            switch (month)
+            {
+                case 02:
+                case 2:
+                    if (isLeap(year))
+                    {
+                        Console.WriteLine("The No. of Days in the month is 29");
+                    }
+                    else
+                        Console.WriteLine("The No. of Days in the month is 28");
+                    break;
+
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    Console.WriteLine("The No. of Days in the month is 31");
+                    break;
+                default:
+                    Console.WriteLine("The No. of Days in the month is 30");
+                    break;
+
+            }
         }
     }
 }

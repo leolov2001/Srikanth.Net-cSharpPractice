6bfd05327279a42b159765d389ff37b50901c9c8
Sunny Leo M <sunny.leo@tammina.com>
2019-02-26 12:47:45 +0530

    Added ReverseNumber
    Ex 3.3: accept a number and display the number in reverse order.  DONE

---
+++ ReverseNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpDemo
+{
+    class ReverseNumber
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Enter the number to display it in reverse order: ");
+            num = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("The Reverse Order is: ");
+            while (num > 0)
+            {
+                //Console.WriteLine();
+                //Console.WriteLine(num % 10);
+                Console.Write(num % 10);
+                num = num / 10;
+            }
+            Console.WriteLine(); Console.WriteLine();
+        }
+    }
+}

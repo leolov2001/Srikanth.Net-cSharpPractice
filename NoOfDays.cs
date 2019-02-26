35fd57b47c1afe342b5c7c1f649ab2cae48ac167
Sunny Leo M <sunny.leo@tammina.com>
2019-02-26 11:56:22 +0530

    Create NoOfDays.cs
    Ex 3.1 started.

---
+++ NoOfDays.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpDemo
+{
+    class NoOfDays
+    {
+        static void Main(string[] args)
+        {
+            int year; string month;
+            Console.Write("Please enter the year: ");
+            year = Int32.Parse(Console.ReadLine());
+            Console.Write("Please enter the month: ");
+            month = (Console.ReadLine());
+        }
+    }
+}

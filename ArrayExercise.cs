83ff4f66ae9a03cea74b9f224d0bf17f73baec59
Sunny Leo M <leolov2001@gmail.com>
2019-02-27 22:05:39 +0530

    starting Ex: 4.4

--- ArrayExercise.cs
+++ ArrayExercise.cs
@@ -10,60 +10,69 @@ namespace cSharpDemo
     {
         static void Main(string[] args)
         {
-            int[] n = new int[10];
-            int[] a = new int[10];
-            int min = 0, max = 9;
-            Console.Write("Please enter 10 numbers randomly: ");
-            for (int i=0; i<10;i++)
-            {
-                n[i]=Int32.Parse(Console.ReadLine());
-            }
-            foreach (int i in n)
-            {
-                if (i % 2 == 0)
-                {
-                    a[min] = i;
-                    min += 1;
-                }
-                else
-                {
-                    a[max] = i;
-                    max -= 1;
-                }
-            }
-            for (int i = 0; i < 10; i++)
-            {
-                if (i == 0)
-                {
-                    Console.Write("[{0},", a[i]);
-                }
-                else
-                {
-                    if (i == 9)
-                    {
-                        Console.WriteLine("{0}]", a[i]);
-                    }
-                    else
+            //int[] n = new int[10];
+            //int[] a = new int[10];
+            //int min = 0, max = 9;
+            //Console.Write("Please enter 10 numbers randomly: ");
+            //for (int i=0; i<10;i++)
+            //{
+            //    n[i]=Int32.Parse(Console.ReadLine());
+            //}
+            //foreach (int i in n)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        a[min] = i;
+            //        min += 1;
+            //    }
+            //    else
+            //    {
+            //        a[max] = i;
+            //        max -= 1;
+            //    }
+            //}
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    if (i == 0)
+            //    {
+            //        Console.Write("[{0},", a[i]);
+            //    }
+            //    else
+            //    {
+            //        if (i == 9)
+            //        {
+            //            Console.WriteLine("{0}]", a[i]);
+            //        }
+            //        else
 
-                    {
-                        Console.Write("{0},", a[i]);
-                    }
-                }
-            }
+            //        {
+            //            Console.Write("{0},", a[i]);
+            //        }
+            //    }
+            //}
+
+            //Console.WriteLine(); Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
+            //Console.WriteLine("Lets do exercise 4.2 - Factorial of a given #");
+            //Console.WriteLine(); Console.WriteLine();
+
+            //Console.Write("Please enter a number: ");
+            //int num2 = Int32.Parse(Console.ReadLine());
+            //int fact = 1;
+            //for (int i = 2; i <= num2; i++)
+            //{
+            //    fact = fact * i;
+            //}
+            //Console.WriteLine(); Console.WriteLine();
+            //Console.WriteLine("Factorial of {0} is {1}.",num2,fact);
+
+            //Console.WriteLine(); Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
+            //Console.WriteLine("Lets do exercise 4.3 - Factorial of a given #");
+            //Console.WriteLine(); Console.WriteLine();
 
             Console.WriteLine(); Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
-
-            Console.WriteLine("Lets do exercise 4.2 - Factorial of a given #");
-
-            Console.Write("Please enter a number: ");
-            int num2 = Int32.Parse(Console.ReadLine());
-            int fact = 1;
-            for (int i = 2; i <= num2; i++)
-            {
-                fact = fact * i;
-            }
+            Console.WriteLine("Lets do exercise 4.4 - Fill Function");
             Console.WriteLine(); Console.WriteLine();
-            Console.WriteLine("Factorial of {0} is {1}.",num2,fact);
+
 
         }
     }

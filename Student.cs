de07bd6210575cc500e138ef1950bad9a8258539
Sunny Leo M <sunny.leo@tammina.com>
2019-03-01 13:54:37 +0530

    Created Student Class.
    Completed Ex 6.1

---
+++ Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpDemo
+{
+    class Student
+    {
+        private int roll; private string name, course;
+
+        public Student(int roll, string name)
+        {
+            this.roll = roll;
+            this.name = name;
+            this.course = "Java";
+        }
+        public Student(int roll, string name, string course)
+        {
+            this.roll = roll;
+            this.name = name;
+            this.course = course;
+        }
+    }
+}

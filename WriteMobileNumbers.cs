622f04735ebbc0891cf7f5dca1516298bdf13129
Sunny Leo M <sunny.leo@tammina.com>
2019-03-13 16:42:34 +0530

    Removed all unnecessary "usings".

---
+++ WriteMobileNumbers.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace cSharpDemo
+{
+    class WriteMobileNumbers
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader();
+        }
+    }
+}

77cecc26f1b586574b41ac2e092830dddf5f0285
Sunny Leo M <sunny.leo@tammina.com>
2019-03-13 11:15:25 +0530

    Update LibraryClassesDemo.cs
    Completed Guessing Game

--- LibraryClassesDemo.cs
+++ LibraryClassesDemo.cs
@@ -54,10 +54,32 @@ namespace cSharpDemo
             }
             
         }
+        public static int checkGuess(int v, int r, int c)
+        {
+            if (v == r)
+            {
+                Console.WriteLine("Awesome! That's a great guess. Please go and celebrate.");
+                c = 4;
+                return c;
+            }
+            else
+            {
+                if (v < r)
+                {
+                    Console.WriteLine("You need to aim higher");
+                    return c;
+                }
+                else
+                {
+                    Console.WriteLine("You need to aim lower");
+                    return c;
+                }
+            }
+        }
 
         static void Main(string[] args)
         {
-            string vertical;
+            //string vertical;
 
             //Ex 8.1
             //Console.Write("Please enter the string to be printed vertically: ");
@@ -85,10 +107,28 @@ namespace cSharpDemo
             //IsCount(vertical);
 
             //Ex8.5
-            Console.Write("Please enter the string to check for 'is': ");
-            vertical = Console.ReadLine();
-
-            IsCount(vertical);
+            int chance = 1, randomNumber;
+            Console.Write("Please enter a number between (1-10): ");
+            int inputNumber= Int32.Parse(Console.ReadLine());
+            Random r = new Random();
+            randomNumber = r.Next(10);
+            do
+            {
+                if (chance > 1)
+                {
+                    Console.Write("Please enter a new number between (1-10): ");
+                    inputNumber = Int32.Parse(Console.ReadLine());
+                }
+                chance = checkGuess(inputNumber, randomNumber, chance);
+                chance++;
+                
+            } while (chance <= 3);
+            if(inputNumber!= randomNumber)
+            {
+                Console.WriteLine("I am sorry, you have run out of your tries. Better luck next time!");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
         }
     }
 }

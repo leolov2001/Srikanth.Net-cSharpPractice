f872ef4c3bb9437c13700ea2aabac069f0904950
Sunny Leo M <sunny.leo@tammina.com>
2019-06-27 11:03:49 +0530

    Last Commit on cSharp

---
+++ SortMobileNumbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpDemo
+{
+    class SortMobileNumbers
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"D:\.NET Practice\cSharpPractice\mobilenumbers.txt");
+            var mobiles = new SortedSet<string>();
+
+            while (true)
+            {
+                string line = sr.ReadLine();
+                if(line == null)
+                {
+                    break;
+                }
+                string[] numbers = line.Split(',');
+                foreach(string n in numbers)
+                {
+                    if (n.Length == 10 && !mobiles.Contains(n))
+                        mobiles.Add(n);
+                }
+            }
+            sr.Close();
+
+            foreach(string n in mobiles)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
---
+++ WordFrequency.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpDemo
+{
+    class WordFrequency
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"D:\.NET Practice\cSharpPractice\toCheckWordFrequency.txt");
+            var frequency = new SortedDictionary<string,int>();
+
+            while (true)
+            {
+                string line = sr.ReadLine();
+                if (line == null)
+                {
+                    break;
+                }
+                string[] words = line.Split(',',' ','.');
+                foreach (string w in words)
+                {
+                    if (frequency.Keys.Contains(w))
+                        frequency[w]++; //increment of the word 'w'
+                    else
+                        frequency[w] = 1; //new word 'w', set count to 1.
+                }
+            }
+            sr.Close();
+
+            foreach (string w in frequency.Keys)
+            {
+                Console.WriteLine($"{w} ---> {frequency[w]}");
+            }
+        }
+    }
+}
